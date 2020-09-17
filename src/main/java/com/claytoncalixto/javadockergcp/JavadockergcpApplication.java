66338ea43d5ee677f0243b785d8742b0e02e7fa3
claytoncalixto <claytonrcalixto@gmail.com>
2020-09-17 17:50:37 -0300

    Created Project
    Created Project

---
+++ src/main/java/com/claytoncalixto/javadockergcp/JavadockergcpApplication.java
@@ -0,0 +1,13 @@
+package com.claytoncalixto.javadockergcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavadockergcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavadockergcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claytoncalixto/javadockergcp/JavadockergcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claytoncalixto.javadockergcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavadockergcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
